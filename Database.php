7801dc9677dff8c03561bcf87d3512fe4d72a9a0
nicoonnicolas <nicolasw.2018@smu.edu.sg>
2022-03-06 23:31:07 +0800

    Create Database.php
    DB Config used in multiple files.

---
+++ Database.php
@@ -0,0 +1,10 @@
+<?php
+
+// Connection Properties to DB
+$host = "localhost";
+$username = "root";
+$password = "";
+$dbName = "cs440_project";
+$link = mysqli_connect($host, $username, $password, $dbName);
+
+?>
